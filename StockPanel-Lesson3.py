c2739bdc5bdc16ef941b7245d5b92fe1e406c835
Saber Nemati <saberiri@yahoo.com>
2019-03-24 17:24:12 +0430

    Redoing lesson 3

--- StockPanel-Lesson3.py
+++ StockPanel-Lesson3.py
@@ -1,8 +1,9 @@
 
-
+import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import jdatetime as jdt
 
 
 def get_max_close(symbol):
@@ -21,20 +22,76 @@ def get_mean_volume(symbol):
     df=pd.read_csv("data/{}.csv".format(symbol))    # read in data
     return df['VolumeTrade'].mean()    # compute and return max
 
+def symbol_to_path(symbol,base_dir="data"):
+    """ Return CSV file path given ticker symbol. """
+    return os.path.join(base_dir,"{}.csv".format(str(symbol)))
+
+def get_data(symbols,dates):
+    """ Read stock data (ClosePrice) for given symbols from CSV files. """
+    df=pd.DataFrame(index=dates)
+    if 'SHARAK' not in symbols:   # add SHARAK for reference, if absent
+        symbols.insert(0,'SHARAK')
+
+    for symbol in symbols:
+        df_temp=pd.read_csv(symbol_to_path(symbol),index_col='GDate',
+                            parse_dates=True,usecols=['GDate','ClosePrice'],
+                            na_values=['nan'])
+        df_temp=df_temp.rename(columns={'ClosePrice':symbol})
+        df=df.join(df_temp)
+        if symbol=='SHARAK':
+            df.dropna(subset=["SHARAK"])
+
+
+    df.drop_duplicates()
+    return df
+
+
 
 def test_run():
 
     """
     LESSON 3
     """
-    ### Part 8-9 ###
-    
+    ### Part 8-11 ###
+    # start_date='2018-03-16'
+    # end_date='2019-03-16'
+    # dates=pd.date_range(start_date,end_date)
+    # # print(dates)
+    # df1=pd.DataFrame(index=dates)
+    # # print(df1)
 
-    ### Part 10-11 ###
+    # dfSHARAK=pd.read_csv("data/SHARAK.csv",index_col='GDate',
+    #                      parse_dates=True,usecols=['GDate','ClosePrice'],
+    #                      na_values=['nan'])
+
+    # dfSHARAK=dfSHARAK.drop_duplicates()
+
+    # dfSHARAK = dfSHARAK.rename(columns={'ClosePrice':'SHARAK'})
+
+    # df1=df1.join(dfSHARAK,how='inner')
+    # # df1=df1.dropna()
+    # # print(df1)
+
+    # symbols=['VBMELLAT','KHODRO','FAMLI']
+    # for symbol in symbols:
+    #     df_temp=pd.read_csv("data/{}.csv".format(symbol),index_col='GDate',
+    #                      parse_dates=True,usecols=['GDate','ClosePrice'],
+    #                      na_values=['nan'])
+    #     df_temp=df_temp.rename(columns={'ClosePrice':symbol})
+
+    #     df1=df1.join(df_temp)
+    #     print(df1)
     
 
     ### Part 12 ###
-    
+    start_date='2018-03-16'
+    end_date='2019-03-16'
+    dates=pd.date_range(start_date,end_date)
+
+    symbols=['VBMELLAT','KHODRO','FAMLI']
+
+    df=get_data(symbols,dates)
+    print(df)
 
     ### Part 13 ###
     
